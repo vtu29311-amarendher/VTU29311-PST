77eef66f6f6f7d187d99bd2b2f689206449b4ddb
vtu29311-amarendher <vtu29311@veltech.edu.in>
2026-08-11 09:45:39 +0530

    Create Java HashSet.java

---
+++ Week 2/Java HashSet.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < t; i++) {
+
+            String a = sc.next();
+            String b = sc.next();
+
+            String pair = a + " " + b;
+
+            set.add(pair);
+
+            System.out.println(set.size());
+        }
+
+        sc.close();
+    }
+}
