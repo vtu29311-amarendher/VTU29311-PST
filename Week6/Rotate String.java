843c4962631288ee2e3ccf0d60d6162005cf14f2
vtu29311-amarendher <vtu29311@veltech.edu.in>
2026-08-25 20:52:16 +0530

    Create Rotate String.java

---
+++ Week6/Rotate String.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        String combined = s + s;
+
+        return combined.contains(goal);
+    }
+}
