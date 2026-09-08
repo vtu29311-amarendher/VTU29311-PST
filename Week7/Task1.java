8b0b1ffe2cc772715d1f67b16b91549f686893f7
vtu29311-amarendher <vtu29311@veltech.edu.in>
2026-09-08 09:19:34 +0530

    Create Task1.java

---
+++ Week7/Task1.java
@@ -0,0 +1,29 @@
+**Problem:** Design a parking system with big, medium, and small spaces. `addCar()` returns `true` if a space is available, otherwise `false`.
+
+
+class ParkingSystem {
+    int big, medium, small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        if (carType == 1 && big > 0) {
+            big--;
+            return true;
+        }
+        if (carType == 2 && medium > 0) {
+            medium--;
+            return true;
+        }
+        if (carType == 3 && small > 0) {
+            small--;
+            return true;
+        }
+        return false;
+    }
+}
+
